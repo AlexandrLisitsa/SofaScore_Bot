837f3a845f25668c70c4fca4027ffc6a67272a8d
WyPik <WyPik28@gmail.com>
2018-07-13 13:37:00 +0300

    HtmlParser
    add class HTML Parser

--- src/com/ria/sofascore_bot/HTMLParser.java
+++ src/com/ria/sofascore_bot/HTMLParser.java
@@ -1,5 +1,71 @@
 package com.ria.sofascore_bot;
 
-public class HTMLParser {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class HTMLParser extends Application {
+
+	
+    public void start(Stage primaryStage) {
+        WebView webview = new WebView();
+        final WebEngine webengine = webview.getEngine();
+        primaryStage.setOpacity(0);
+        webengine.getLoadWorker().stateProperty().addListener(
+        
+                new ChangeListener<Worker.State>() {
+
+                    public void changed(ObservableValue ov, Worker.State oldState, Worker.State newState) {
+                        if (newState == Worker.State.SUCCEEDED) {
+                            Document doc = webengine.getDocument();
+                            try {
+                                Transformer transformer = TransformerFactory.newInstance().newTransformer();
+                                transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+                                transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+                                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+                                transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+                                transformer.setOutputProperty("{https://www.sofascore.com/ru/tennis/livescore}", "4");
+
+                                transformer.transform(new DOMSource(doc),
+                                        new StreamResult(new OutputStreamWriter(new FileOutputStream(new File("C:\\Users\\wypik\\Desktop\\new 1.txt")), "UTF-8")));
+                            } catch (Exception ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    }
+                });
+        webengine.load("https://www.sofascore.com/ru/tennis/livescore");
+        Scene view = new Scene(webview,800,600);
+        primaryStage.setScene(view);
+        primaryStage.setTitle("sofascore_parser");
+        primaryStage.show();
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(5000),e->{
+        	webengine.reload();
+        });
+        Timeline timeline = new Timeline(keyFrame);
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+        
+    }
 
 }
